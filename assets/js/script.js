de23a448e45f3d3318ec53c50512c46b8d325d66
Daya Suvagiya <diya.jivani.diji@gmail.com>
2023-06-02 23:02:04 +0200

    first question and answer completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,14 +7,57 @@ const questions = [
         optionD: "7 days",
         correctOption: "optionD"
     }]
+
+    let scrore = 0;
     
+    /*answer function*/ 
+    function onClickAnswer(){
+        
+        console.log('bharat')
+    }
+
+
+
     function buildQuizze(){
         let quizz= document.getElementsByClassName('quizz-question')[0];
         let questionImage= document.createElement('img');
         questionImage.setAttribute('src',questions[0].question);
-        questionImage.setAttribute('width','100px')
-        questionImage.setAttribute('height','100px')
+        questionImage.setAttribute('width','100px');
+        questionImage.setAttribute('height','100px');
         quizz.appendChild(questionImage);
-    }
+
+        let optionContainer=document.createElement('div');
+        optionContainer.setAttribute('id','answer-button');
+
+        let optionA=document.createElement('button');
+        optionA.innerHTML=questions[0].optionA;
+        optionA.setAttribute('class','answer');
+        optionA.addEventListener('click', onClickAnswer);
+
+        let optionB=document.createElement('button');
+        optionB.innerHTML=questions[0].optionB;
+        optionB.setAttribute('class','answer');
+        optionB.addEventListener('click', onClickAnswer);
+
+        let optionC=document.createElement('button');
+        optionC.innerHTML=questions[0].optionC;
+        optionC.setAttribute('class','answer');
+        optionC.addEventListener('click', onClickAnswer);
+
+        let optionD=document.createElement('button');
+        optionD.innerHTML=questions[0].optionD;
+        optionD.setAttribute('class','answer');
+        optionD.addEventListener('click', onClickAnswer);
+
+        optionContainer.appendChild(optionA);
+        optionContainer.appendChild(optionB);
+        optionContainer.appendChild(optionC);
+        optionContainer.appendChild(optionD);
+
+        quizz.appendChild(optionContainer);
+
+        }
 
     buildQuizze();
+
+    
