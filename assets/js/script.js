8915f7a014c3826a1351c716c18c57cd1b99ac12
Daya Suvagiya <diya.jivani.diji@gmail.com>
2023-06-03 11:45:19 +0200

    set four questions and functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,22 +6,60 @@ const questions = [
         optionC: "5 days",
         optionD: "7 days",
         correctOption: "optionD"
-    }]
+    },
+    {
+        question: "../BangBang/assets/images/strobery.png",
+        optionA: "11 days",
+        optionB: "14 days",
+        optionC: "5 days",
+        optionD: "7 days",
+        correctOption: "optionD"
+    },
+    {
+        question: "../BangBang/assets/images/strobery.png",
+        optionA: "12 days",
+        optionB: " days",
+        optionC: "5 days",
+        optionD: "7 days",
+        correctOption: "optionD"
+    },
+    {
+        question: "../BangBang/assets/images/strobery.png",
+        optionA: "13 days",
+        optionB: "14 days",
+        optionC: "5 days",
+        optionD: "7 days",
+        correctOption: "optionD"
+    }
+]
 
     let scrore = 0;
+    let currentQuestion = 0;
+    let isClick = false;
     
     /*answer function*/ 
-    function onClickAnswer(){
-        
-        console.log('bharat')
+    function onClickAnswer(evt){
+        if(!isClick) {
+            if(evt.currentTarget.correctOption === questions[currentQuestion].correctOption) {
+                scrore = scrore + 1;
+                evt.currentTarget.style.backgroundColor = 'green';
+                let scoreElement = document.getElementById('score');
+                scoreElement.innerHTML = scrore;
+                isClick = true;
+            } else {
+                evt.currentTarget.style.backgroundColor = 'red';
+            }
+        }
+       
     }
 
 
 
     function buildQuizze(){
         let quizz= document.getElementsByClassName('quizz-question')[0];
+        console.log(quizz)
         let questionImage= document.createElement('img');
-        questionImage.setAttribute('src',questions[0].question);
+        questionImage.setAttribute('src',questions[currentQuestion].question);
         questionImage.setAttribute('width','100px');
         questionImage.setAttribute('height','100px');
         quizz.appendChild(questionImage);
@@ -30,23 +68,24 @@ const questions = [
         optionContainer.setAttribute('id','answer-button');
 
         let optionA=document.createElement('button');
-        optionA.innerHTML=questions[0].optionA;
+        optionA.innerHTML=questions[currentQuestion].optionA;
         optionA.setAttribute('class','answer');
         optionA.addEventListener('click', onClickAnswer);
 
         let optionB=document.createElement('button');
-        optionB.innerHTML=questions[0].optionB;
+        optionB.innerHTML=questions[currentQuestion].optionB;
         optionB.setAttribute('class','answer');
         optionB.addEventListener('click', onClickAnswer);
 
         let optionC=document.createElement('button');
-        optionC.innerHTML=questions[0].optionC;
+        optionC.innerHTML=questions[currentQuestion].optionC;
         optionC.setAttribute('class','answer');
         optionC.addEventListener('click', onClickAnswer);
 
         let optionD=document.createElement('button');
-        optionD.innerHTML=questions[0].optionD;
+        optionD.innerHTML=questions[currentQuestion].optionD;
         optionD.setAttribute('class','answer');
+        optionD.correctOption='optionD';
         optionD.addEventListener('click', onClickAnswer);
 
         optionContainer.appendChild(optionA);
@@ -59,5 +98,15 @@ const questions = [
         }
 
     buildQuizze();
+
+    function onNextQuestion() {
+        currentQuestion = currentQuestion + 1;
+        let questionNumber = document.getElementById('question-number');
+        questionNumber.innerHTML = 'question: ' + currentQuestion;
+        isClick = false;
+        buildQuizze();
+    }
+    let nextButton = document.getElementById('next');
+    nextButton.addEventListener('click', onNextQuestion);
 
     
