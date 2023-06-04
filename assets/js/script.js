7465168f998ec7b8d47432d1139b1e81951a4d1c
Daya Suvagiya <diya.jivani.diji@gmail.com>
2023-06-04 15:47:43 +0200

    Set all questions and answers in js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,46 +1,115 @@
 const questions = [
     {
-        question: "../BangBang/assets/images/strobery.png",
-        optionA: "10 days",
-        optionB: "14 days",
-        optionC: "5 days",
-        optionD: "7 days",
-        correctOption: "optionD"
+        question: "../BangBang/assets/images/srawberry.png",
+        options : [
+            "Die Strawberry",
+            "Die Erdbeere",
+            "Die Kirsche",
+            "Der Apfel",
+        ],
+        correctOption: [1],
     },
     {
-        question: "../BangBang/assets/images/strobery.png",
-        optionA: "11 days",
-        optionB: "14 days",
-        optionC: "5 days",
-        optionD: "7 days",
-        correctOption: "optionD"
+        question: "../BangBang/assets/images/bicycle.png",
+        options : [
+            "Das Fahrrad", 
+            "Das Auto",
+            "Der Bus",
+            "Das Motorrad"
+        ],
+        correctOption: [0]
     },
     {
-        question: "../BangBang/assets/images/strobery.png",
-        optionA: "12 days",
-        optionB: " days",
-        optionC: "5 days",
-        optionD: "7 days",
-        correctOption: "optionD"
+        question: "../BangBang/assets/images/freanchfries.png",
+        options : [
+            "Die Kartoffel", 
+            "Der Burger",
+            "Die Gurke",
+            "Die Kartoffel"
+        ],
+        correctOption: [3]
     },
     {
-        question: "../BangBang/assets/images/strobery.png",
-        optionA: "13 days",
-        optionB: "14 days",
-        optionC: "5 days",
-        optionD: "7 days",
-        correctOption: "optionD"
+        question: "../BangBang/assets/images/bag.png",
+        options : [
+            "Der Geldbeutel", 
+            "Der Koffer",
+            "Die Tasche",
+            "Das Gepäck"
+        ],
+        correctOption: [2]
+    },
+    {
+        question: "../BangBang/assets/images/cake.png",
+        options : [
+            "Der Käse", 
+            "Die Schokolade",
+            "Der Kuchen",
+            "Die Butter"
+        ],
+        correctOption: [2]
+    },
+    {
+        question: "../BangBang/assets/images/dog.png",
+        options : [
+            "Der Hund", 
+            "Das Reh",
+            "Das Schwein",
+            "Die Katze"
+        ],
+        correctOption: [0]
+    },
+    {
+        question: "../BangBang/assets/images/flower.png",
+        options : [
+            "Das Gemüse", 
+            "Das Obst",
+            "Die Pflanze",
+            "Die Blumen"
+        ],
+        correctOption: [3]
+    },
+    {
+        question: "../BangBang/assets/images/lemon.png",
+        options : [
+            "Die Gurke", 
+            "Die Zitrone",
+            "Die Zucchini",
+            "Der Mais"
+        ],
+        correctOption: [1]
+    },
+    {
+        question: "../BangBang/assets/images/plant.png",
+        options : [
+            "Die Pflanze", 
+            "Der Baum",
+            "Der Topf",
+            "Die Blume"
+        ],
+        correctOption: [0]
+    },
+    {
+        question: "../BangBang/assets/images/TV.png",
+        options : [
+            "Das Fenster", 
+            "Der Schrank",
+            "Der Schrank",
+            "Der Fernseher"
+        ],
+        correctOption: [0]
     }
+
 ]
 
     let scrore = 0;
-    let currentQuestion = 0;
+    let currentQuestion = 1;
     let isClick = false;
     
     /*answer function*/ 
     function onClickAnswer(evt){
         if(!isClick) {
-            if(evt.currentTarget.correctOption === questions[currentQuestion].correctOption) {
+            if(evt.currentTarget.correctOption === questions[currentQuestion - 1].correctOption) {
                 scrore = scrore + 1;
                 evt.currentTarget.style.backgroundColor = 'green';
                 let scoreElement = document.getElementById('score');
@@ -59,31 +128,31 @@ const questions = [
         let quizz= document.getElementsByClassName('quizz-question')[0];
         console.log(quizz)
         let questionImage= document.createElement('img');
-        questionImage.setAttribute('src',questions[currentQuestion].question);
-        questionImage.setAttribute('width','100px');
-        questionImage.setAttribute('height','100px');
+        questionImage.setAttribute('src',questions[currentQuestion-1].question);
+        questionImage.setAttribute('width','200px');
+        questionImage.setAttribute('height','150px');
         quizz.appendChild(questionImage);
 
         let optionContainer=document.createElement('div');
         optionContainer.setAttribute('id','answer-button');
 
         let optionA=document.createElement('button');
-        optionA.innerHTML=questions[currentQuestion].optionA;
+        optionA.innerHTML=questions[currentQuestion-1].optionA;
         optionA.setAttribute('class','answer');
         optionA.addEventListener('click', onClickAnswer);
 
         let optionB=document.createElement('button');
-        optionB.innerHTML=questions[currentQuestion].optionB;
+        optionB.innerHTML=questions[currentQuestion-1].optionB;
         optionB.setAttribute('class','answer');
         optionB.addEventListener('click', onClickAnswer);
 
         let optionC=document.createElement('button');
-        optionC.innerHTML=questions[currentQuestion].optionC;
+        optionC.innerHTML=questions[currentQuestion-1].optionC;
         optionC.setAttribute('class','answer');
         optionC.addEventListener('click', onClickAnswer);
 
         let optionD=document.createElement('button');
-        optionD.innerHTML=questions[currentQuestion].optionD;
+        optionD.innerHTML=questions[currentQuestion-1].optionD;
         optionD.setAttribute('class','answer');
         optionD.correctOption='optionD';
         optionD.addEventListener('click', onClickAnswer);
@@ -105,9 +174,7 @@ const questions = [
         questionNumber.innerHTML = 'question: ' + currentQuestion;
         isClick = false;
         const quizzContainer = document.getElementsByClassName('quizz-question')[0];
-        console.log(':::', quizzContainer.firstElementChild);
-        quizzContainer.removeChilde(quizzContainer.firstElementChild)
-        console.log(imgTag);
+        quizzContainer.innerHTML = '';
         buildQuizze();
     }
     let nextButton = document.getElementById('next');
